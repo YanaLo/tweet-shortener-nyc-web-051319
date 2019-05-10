8c56114fb9e0c1e1d7241191f83510fe0abd293d
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 10:38:56 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+def word_substituter(words)
+  subs=words.split(" ")
+  another_word=[]
+  subs.each do |word|
+    case word.downcase
+    when "hello"
+      another_word<<"hi"
+      when "to","two","too"
+        another_word<<"2"
+        when "for", "four" 
+        another_word<<"4"
+        when "be"
+        another_word<<"b"
+        when "you"
+        another_word<<"u"
+        when "at"
+        another_word<<"@"
+        when "and"
+        another_word<<"&"
+      else 
+      another_word<<word
+  end
+end
+  another_word.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+array.each{|i| puts word_substituter(i)}
+end
+
+def selective_tweet_shortener(array)
+if array.length>140
+word_substituter(array)
+else 
+array
+end
+end
+
+def shortened_tweet_truncator(array)
+if array.length>140
+word_substituter(array)
+array[1..137]+"..."
+else
+array
+end
+end
+
+
+
